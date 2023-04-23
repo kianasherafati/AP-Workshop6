d8361b2a0ed8d6c7079b3e14266c6097841f4832
kiana <kiana.sherafati@gmail.com>
2023-04-24 03:38:10 +0430

    feat : completing Bird class

--- src/Bird.java
+++ src/Bird.java
@@ -1,3 +1,12 @@
 public abstract class Bird extends Animal{
+    private int heightOfFlight;
 
+    public Bird(String name, int age, int heightOfFlight) {
+        super(name, age);
+        this.heightOfFlight = heightOfFlight;
+    }
+
+    public int getHeightOfFlight() {
+        return heightOfFlight;
+    }
 }
