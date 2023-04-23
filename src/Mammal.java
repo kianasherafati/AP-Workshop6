d1be693de9b0f30bfcd01e1c616240dae00a9d9c
kiana <kiana.sherafati@gmail.com>
2023-04-24 03:39:35 +0430

    feat : completing Mammal class

--- src/Mammal.java
+++ src/Mammal.java
@@ -1,3 +1,12 @@
 public abstract class Mammal extends Animal {
+    private int speed;
 
+    public Mammal(String name, int age, int speed) {
+        super(name, age);
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
