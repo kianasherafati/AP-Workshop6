c7640429b32c8d300b2edd2a6ac44425919a93cc
kiana <kiana.sherafati@gmail.com>
2023-04-24 03:42:44 +0430

    feat : completing Eagle class

--- src/Eagle.java
+++ src/Eagle.java
@@ -1,2 +1,12 @@
 public class Eagle extends Bird implements Hunter{
+    private String eagleSpecialty;
+
+    public Eagle(String name, int age, int heightOfFlight, String eagleSpecialty) {
+        super(name, age, heightOfFlight);
+        this.eagleSpecialty = eagleSpecialty;
+    }
+
+    public void show(){
+        System.out.println(getName() + "," + getAge() + "," + getHeightOfFlight() + "," + eagleSpecialty);
+    }
 }
