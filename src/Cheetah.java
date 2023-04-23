09ac787b3892cee0e0ab27fb173d4b59ca5d4277
kiana <kiana.sherafati@gmail.com>
2023-04-24 03:41:09 +0430

    feat : completing Cheetah class

--- src/Cheetah.java
+++ src/Cheetah.java
@@ -1,3 +1,12 @@
 public class Cheetah extends Mammal implements Hunter{
+    private String cheetahSpecialty;
 
+    public Cheetah(String name, int age, int speed, String cheetahSpecialty) {
+        super(name, age, speed);
+        this.cheetahSpecialty = cheetahSpecialty;
+    }
+
+    public void show(){
+        System.out.println(getName() + "," + getAge() + "," + getSpeed() + "," + cheetahSpecialty);
+    }
 }
